3b9d429e2a8151300ad7be5c98a261645d3d591e
= <abhisingh30800@gmail.com>
2023-01-31 11:06:19 +0530

    scroll animation in the heading and gallery

--- main.js
+++ main.js
@@ -13,3 +13,30 @@ gsap.fromTo(
   { x: 0, opacity: 1, duration: 2 },
   "<"
 );
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".gallery--heading", {
+  scrollTrigger: {
+    trigger: ".gallery--heading",
+    start: "0% 90%",
+    end: "bottom bottom",
+    // markers: true,
+    scrub: 3,
+    toggleActions: "restart pause reverse pause",
+  },
+
+  x: -400,
+  opacity: 0,
+  duration: 3,
+  ease: "none",
+});
+
+// gsap.utils.toArray(".gallery").forEach((gallery) => {
+//   ScrollTrigger.create({
+//     trigger: gallery,
+//     start: "top top",
+//     pin: true,
+//     pinSpacing: false,
+//   });
+// });
